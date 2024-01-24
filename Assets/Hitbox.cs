0c3715fec44651453573a677392e13645d669a4e
pesal <faishalsyahbari@gmail.com>
2024-01-24 18:29:56 +0700

    initial commit

---
+++ Assets/Hitbox.cs
@@ -0,0 +1,23 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public Collision collision;
+    public PlayerScript player;
+    public Stats entityStats;
+    public string target;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (entityStats.health <= 0)
+        {
+            player.enabled = false;
+        }
+        if (collisionInfo.collider.tag == target)
+        {
+            entityStats.health -= 25;
+        }
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,43 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddForce(0,0, 1000 * Time.deltaTime);
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 100 * Time.deltaTime, 500 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space)){
+            rb.AddForce(0, 650 * Time.deltaTime, 0);
+        }
+    }
+}
